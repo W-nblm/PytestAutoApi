872dd153804eb26d3039680524b69f7770b83325
wyk <834532523@qq.com>
2025-06-30 17:22:44 +0800

    优化redis

--- redis_main.py
+++ redis_main.py
@@ -5,9 +5,11 @@ from utils.other_tools.aes_encrypt import AESUtils
 if __name__ == "__main__":
     redis_helper = RedisHelper(db=5)
     res = redis_helper.get_cached_messages(
-        topic_prefix="dev:op:shadowProp:d-f5701968-w3o1q6nf"
+        topic_prefix="dev:op:shadowProp:d-8d8b4768-ns6aoiho"
     )
-    res = res["dev:op:shadowProp:d-f5701968-w3o1q6nf"]
+    res = res["dev:op:shadowProp:d-8d8b4768-ns6aoiho"]
     print(res)
     aes_encrypt = AESUtils()
-    aes_encrypt.encrypt(res['battery_quantity'],)
+    aes_encrypt.encrypt(
+        res["battery_quantity"],
+    )
